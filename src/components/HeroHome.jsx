517ccc6a5763e4a79e67ecd2704f708746d79926
Dev Ahluwalia <devahluwalia34@gmail.com>
2026-01-24 16:54:59 -0600

    remove underline from hyperlink

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -27,7 +27,6 @@ const HeroHome = () => {
                 href="https://anhaidgroup.github.io/magellan/"
                 style={{
                   textDecoration: 'none',
-                  borderBottom: '1px dotted #9ca3af',
                 }}
               >
                 decade of research at the University of Wisconsin-Madison
